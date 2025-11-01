3ae2135ecf3b5ca824aa2c3d53536719d878dc80
arraisi <arraisi@icloud.com>
2025-11-01 15:48:18 +0700

    Refactor ServiceBookingRecall model to update ID type and field names for consistency

--- internal/domain/servicebooking_recall.go
+++ internal/domain/servicebooking_recall.go
@@ -7,11 +7,11 @@ import (
 )
 
 type ServiceBookingRecall struct {
-	ID                int64     `db:"i_id"`
+	ID                string    `db:"i_id"`
 	ServiceBookingID  string    `db:"i_service_booking_id"`
 	RecallID          string    `db:"i_recall_id"`
 	RecallDate        string    `db:"d_recall_date"`
-	RecallDescription string    `db:"n_recall_description"`
+	RecallDescription string    `db:"e_recall_description"`
 	AffectedPart      string    `db:"c_affected_part"`
 	CreatedAt         time.Time `db:"d_created_at"`
 	CreatedBy         string    `db:"c_created_by"`
@@ -21,22 +21,22 @@ type ServiceBookingRecall struct {
 
 // TableName returns the database table name for the ServiceBookingRecall model
 func (sbr *ServiceBookingRecall) TableName() string {
-	return "dbo.tm_service_booking_recalls"
+	return "dbo.tr_service_booking_recall"
 }
 
 // Columns returns the list of database columns for the ServiceBookingRecall model
 func (sbr *ServiceBookingRecall) Columns() []string {
 	return []string{
 		"i_id",
+		"i_service_booking_id",
 		"i_recall_id",
 		"d_recall_date",
-		"n_recall_description",
+		"e_recall_description",
 		"c_affected_part",
 		"d_created_at",
 		"c_created_by",
 		"d_updated_at",
 		"c_updated_by",
-		"i_service_booking_id",
 	}
 }
 
@@ -44,15 +44,15 @@ func (sbr *ServiceBookingRecall) Columns() []string {
 func (sbr *ServiceBookingRecall) SelectColumns() []string {
 	return []string{
 		"CAST(i_id AS NVARCHAR(36)) as i_id",
+		"CAST(i_service_booking_id AS NVARCHAR(36)) as i_service_booking_id",
 		"CAST(i_recall_id AS NVARCHAR(36)) as i_recall_id",
 		"d_recall_date",
-		"n_recall_description",
+		"e_recall_description",
 		"c_affected_part",
 		"d_created_at",
 		"c_created_by",
 		"d_updated_at",
 		"c_updated_by",
-		"CAST(i_service_booking_id AS NVARCHAR(36)) as i_service_booking_id",
 	}
 }
 
@@ -61,11 +61,14 @@ func (sbr *ServiceBookingRecall) ToCreateMap() ([]string, []interface{}) {
 	columns := make([]string, 0, len(sbr.Columns()))
 	values := make([]interface{}, 0, len(sbr.Columns()))
 
-	//i_id
 	id := uuid.NewString()
 	columns = append(columns, "i_id")
 	values = append(values, id)
 
+	if sbr.ServiceBookingID != "" {
+		columns = append(columns, "i_service_booking_id")
+		values = append(values, sbr.ServiceBookingID)
+	}
 	if sbr.RecallID != "" {
 		columns = append(columns, "i_recall_id")
 		values = append(values, sbr.RecallID)
@@ -75,26 +78,17 @@ func (sbr *ServiceBookingRecall) ToCreateMap() ([]string, []interface{}) {
 		values = append(values, sbr.RecallDate)
 	}
 	if sbr.RecallDescription != "" {
-		columns = append(columns, "n_recall_description")
+		columns = append(columns, "e_recall_description")
 		values = append(values, sbr.RecallDescription)
 	}
 	if sbr.AffectedPart != "" {
 		columns = append(columns, "c_affected_part")
 		values = append(values, sbr.AffectedPart)
 	}
-	columns = append(columns, "d_created_at")
-	values = append(values, sbr.CreatedAt)
 	columns = append(columns, "c_created_by")
 	values = append(values, sbr.CreatedBy)
-	columns = append(columns, "d_updated_at")
-	values = append(values, sbr.UpdatedAt)
 	columns = append(columns, "c_updated_by")
 	values = append(values, sbr.UpdatedBy)
-
-	if sbr.ServiceBookingID != "" {
-		columns = append(columns, "i_service_booking_id")
-		values = append(values, sbr.ServiceBookingID)
-	}
 
 	return columns, values
 }
@@ -102,6 +96,9 @@ func (sbr *ServiceBookingRecall) ToCreateMap() ([]string, []interface{}) {
 func (sbr *ServiceBookingRecall) ToUpdateMap() map[string]interface{} {
 	updateMap := make(map[string]interface{})
 
+	if sbr.ServiceBookingID != "" {
+		updateMap["i_service_booking_id"] = sbr.ServiceBookingID
+	}
 	if sbr.RecallID != "" {
 		updateMap["i_recall_id"] = sbr.RecallID
 	}
@@ -109,17 +106,13 @@ func (sbr *ServiceBookingRecall) ToUpdateMap() map[string]interface{} {
 		updateMap["d_recall_date"] = sbr.RecallDate
 	}
 	if sbr.RecallDescription != "" {
-		updateMap["n_recall_description"] = sbr.RecallDescription
+		updateMap["e_recall_description"] = sbr.RecallDescription
 	}
 	if sbr.AffectedPart != "" {
 		updateMap["c_affected_part"] = sbr.AffectedPart
 	}
-	updateMap["d_updated_at"] = sbr.UpdatedAt
+	updateMap["d_updated_at"] = time.Now()
 	updateMap["c_updated_by"] = sbr.UpdatedBy
-
-	if sbr.ServiceBookingID != "" {
-		updateMap["i_service_booking_id"] = sbr.ServiceBookingID
-	}
 
 	return updateMap
 }
