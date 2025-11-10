a63b3a7b06110e2273a77429e0588f6d3ce3ec20
aprianfirlanda <aprianfirlanda@gmail.com>
2025-11-10 14:37:25 +0700

    feat(webhook toyota id): add service function for upsert customer from domain model(UpsertCustomerV2)

---
+++ internal/service/customer/upsert_customer_v2.go
@@ -0,0 +1,37 @@
+package customer
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/arraisi/hcm-be/internal/domain"
+
+	"github.com/arraisi/hcm-be/internal/domain/dto/customer"
+	"github.com/jmoiron/sqlx"
+)
+
+func (s *service) UpsertCustomerV2(ctx context.Context, tx *sqlx.Tx, req domain.Customer) (string, error) {
+	customerData, err := s.repo.GetCustomer(ctx, customer.GetCustomerRequest{
+		OneAccountID: req.OneAccountID,
+	})
+	if err == nil {
+		// Found → update
+		err = s.repo.UpdateCustomer(ctx, tx, req)
+		if err != nil {
+			return customerData.ID, err
+		}
+		return customerData.ID, nil
+	}
+
+	// Not found → create
+	if errors.Is(err, sql.ErrNoRows) {
+		err := s.repo.CreateCustomer(ctx, tx, &req)
+		if err != nil {
+			return req.ID, err
+		}
+		return req.ID, nil
+	}
+
+	// other error
+	return customerData.ID, err
+}
