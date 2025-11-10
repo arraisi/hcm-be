c069e276951325ab8e19f77f5d4a8b69e47cdeec
aprianfirlanda <aprianfirlanda@gmail.com>
2025-11-10 14:43:55 +0700

    feat(webhook toyota id): add service function for upsert customer vehicle from domain model(UpsertCustomerVehicleV2)

---
+++ internal/service/customervehicle/upsert_customer_vehicle_v2.go
@@ -0,0 +1,43 @@
+package customer
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/arraisi/hcm-be/internal/domain"
+
+	"github.com/arraisi/hcm-be/internal/domain/dto/customervehicle"
+	"github.com/arraisi/hcm-be/pkg/utils"
+	"github.com/jmoiron/sqlx"
+)
+
+func (s *service) UpsertCustomerVehicleV2(ctx context.Context, tx *sqlx.Tx, customerID string, req domain.CustomerVehicle) (string, error) {
+	existingCV, err := s.repo.GetCustomerVehicle(ctx, customervehicle.GetCustomerVehicleRequest{
+		Vin:          utils.ToPointer(req.Vin),
+		PoliceNumber: utils.ToPointer(req.PoliceNumber),
+	})
+	if err == nil {
+		// Found → update
+		req.ID = existingCV.ID
+		req.CustomerID = customerID
+
+		err = s.repo.UpdateCustomerVehicle(ctx, tx, req)
+		if err != nil {
+			return existingCV.ID, err
+		}
+		return existingCV.ID, nil
+	}
+
+	// Not found → create
+	if errors.Is(err, sql.ErrNoRows) {
+		req.CustomerID = customerID
+		err := s.repo.CreateCustomerVehicle(ctx, tx, &req)
+		if err != nil {
+			return req.ID, err
+		}
+		return req.ID, nil
+	}
+
+	// other error
+	return existingCV.ID, err
+}
