ec6be30b9d3fa29646ab1213d061d6bf366e9671
aprianfirlanda <aprianfirlanda@gmail.com>
2025-11-12 07:26:45 +0700

    feat(webhook customer reminder): add layer repository

---
+++ internal/repository/customerreminder/create_customer_reminder.go
@@ -0,0 +1,32 @@
+package customerreminder
+
+import (
+	"context"
+	"github.com/arraisi/hcm-be/internal/domain"
+	"github.com/elgris/sqrl"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+)
+
+func (r *repository) CreateCustomer(ctx context.Context, tx *sqlx.Tx, req *domain.CustomerReminder) error {
+	columns, values := req.ToCreateMap()
+
+	// Generate a new UUID for the customer ID
+	req.ID = uuid.NewString()
+	columns = append(columns, "i_id")
+	values = append(values, req.ID)
+
+	query, args, err := sqrl.Insert(req.TableName()).
+		Columns(columns...).
+		Values(values...).ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.ExecContext(ctx, r.db.Rebind(query), args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
