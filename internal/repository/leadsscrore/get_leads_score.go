5cc101e3bfcdd9562561498ed5d82e8376976a42
aprianfirlanda <aprianfirlanda@gmail.com>
2025-12-03 06:31:35 +0700

    Introduce `LeadsScore` repository with CRUD operations
    - Added `CreateLeadScore`, `GetLeadScore`, and `UpdateLeadScore` methods in the repository.
    - Defined `LeadsScore` domain model with table mappings and utility methods.
    - Integrated support for `tm_leads_score` table operations.

---
+++ internal/repository/leadsscrore/get_leads_score.go
@@ -0,0 +1,28 @@
+package usedcar
+
+import (
+	"context"
+	"github.com/arraisi/hcm-be/internal/domain"
+	"github.com/elgris/sqrl"
+)
+
+func (r *repository) GetLeadScore(ctx context.Context, leadsID string) (domain.LeadsScore, error) {
+	var model domain.LeadsScore
+
+	query := sqrl.Select(model.SelectColumns()...).
+		From(model.TableName()).
+		Where(sqrl.Eq{"i_leads_id": leadsID})
+
+	sqlQuery, args, err := query.ToSql()
+	if err != nil {
+		return model, err
+	}
+
+	sqlQuery = r.db.Rebind(sqlQuery)
+	err = r.db.GetContext(ctx, &model, sqlQuery, args...)
+	if err != nil {
+		return model, err
+	}
+
+	return model, nil
+}
